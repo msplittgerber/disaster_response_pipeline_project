fe02280eb8d8512e9b7da5a9de940a8ed1c0cbf0
maike <maike-splittgerber@web.de>
2023-05-01 13:44:13 +0200

    Added Machine Learning Pipeline

--- data/process_data.py
+++ data/process_data.py
@@ -35,7 +35,7 @@ def clean_data(df):
 
 def save_data(df, database_filename):
     engine = create_engine("sqlite:///"+database_filename)
-    df.to_sql('DisasterDF', engine, index=False)
+    df.to_sql('DisasterDF', engine, index=False, if_exists="replace")
     return True  
 
 
--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,16 +1,59 @@
 import sys
+import pandas as pd
+import numpy as np
+from sqlalchemy import create_engine, text
+import re
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.tokenize import sent_tokenize
+nltk.download('punkt')
+nltk.download('wordnet')
 
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.feature_extraction.text import TfidfTransformer, CountVectorizer
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import GradientBoostingClassifier, RandomForestClassifier, AdaBoostClassifier
+
+
+# to do: add starting verb extractor?
 
 def load_data(database_filepath):
-    pass
+    # load data from database
+    engine = create_engine("sqlite:///"+database_filepath)
+    df = pd.read_sql_table('DisasterDF', engine)
+    X = df['message']
+    Y = df.iloc[:,4:]
+    category_names = Y.columns
+    return X, Y, category_names
 
 
 def tokenize(text):
-    pass
+    # Normalize text
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text.lower())
+    # Extract the word tokens from the provided text
+    tokens = nltk.word_tokenize(text)
+    #Lemmanitizer to remove inflectional and derivationally related forms of a word
+    lemmatizer = nltk.WordNetLemmatizer()
+
+    # List of clean tokens
+    clean_tokens = [lemmatizer.lemmatize(w).lower().strip() for w in tokens]
+    return clean_tokens
 
 
 def build_model():
-    pass
+    pipeline = Pipeline([
+        ('features', FeatureUnion([
+
+            ('text_pipeline', Pipeline([
+                ('count_vectorizer', CountVectorizer(tokenizer=tokenize)),
+                ('tfidf_transformer', TfidfTransformer())
+            ]))
+        ])),
+
+        ('classifier', MultiOutputClassifier(AdaBoostClassifier()))
+    ])
+    return pipeline
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
@@ -18,7 +61,7 @@ def evaluate_model(model, X_test, Y_test, category_names):
 
 
 def save_model(model, model_filepath):
-    pass
+    pickle.dump(model, open(model_filepath, 'wb'))
 
 
 def main():
