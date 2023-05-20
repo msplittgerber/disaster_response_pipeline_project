c427aacb66b5841bdb64a7929891514891445907
maike <maike-splittgerber@web.de>
2023-05-20 14:18:10 +0200

    Updated Machine Learning Pipeline

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -14,12 +14,27 @@ from sklearn.feature_extraction.text import TfidfTransformer, CountVectorizer
 from sklearn.multioutput import MultiOutputClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import GradientBoostingClassifier, RandomForestClassifier, AdaBoostClassifier
+from sklearn.metrics import classification_report
+from sklearn.model_selection import GridSearchCV
+
+import pickle
 
 
-# to do: add starting verb extractor?
 
 def load_data(database_filepath):
-    # load data from database
+    """
+    Load data from a SQLite database.
+
+    Parameters:
+        database_filepath (str): Filepath of the SQLite database.
+
+    Returns:
+        tuple: A tuple containing the following elements:
+            X (pandas.Series): The input messages.
+            Y (pandas.DataFrame): The output labels.
+            category_names (list): The list of category names.
+
+    """
     engine = create_engine("sqlite:///"+database_filepath)
     df = pd.read_sql_table('DisasterDF', engine)
     X = df['message']
@@ -29,6 +44,17 @@ def load_data(database_filepath):
 
 
 def tokenize(text):
+    """
+    Tokenize and preprocess text data.
+
+    Parameters:
+        text (str): Input text to be tokenized.
+
+    Returns:
+        list: A list of preprocessed tokens.
+
+    """
+
     # Normalize text
     text = re.sub(r"[^a-zA-Z0-9]", " ", text.lower())
     # Extract the word tokens from the provided text
@@ -42,6 +68,13 @@ def tokenize(text):
 
 
 def build_model():
+    """
+    Build a machine learning model using a pipeline and perform grid search.
+
+    Returns:
+        GridSearchCV: A GridSearchCV object configured with the pipeline and parameter grid.
+
+    """
     pipeline = Pipeline([
         ('features', FeatureUnion([
 
@@ -53,14 +86,48 @@ def build_model():
 
         ('classifier', MultiOutputClassifier(AdaBoostClassifier()))
     ])
-    return pipeline
+
+    # Define the parameter grid for grid search
+    param_grid = {
+        'features__text_pipeline__count_vectorizer__max_features': [1000, 5000],
+        'classifier__estimator__n_estimators': [50, 100],
+        'classifier__estimator__learning_rate': [0.1, 0.5]
+    }
+
+    # Perform grid search
+    cv = GridSearchCV(pipeline, param_grid, cv=3)
+
+    return cv
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    """
+    Evaluate model performance measured by precision, recall, f1-score, support.
+
+    Parameters:
+        model: The trained machine learning model.
+        X_test (pandas.Series): The input features of the test dataset.
+        Y_test (pandas.DataFrame): The true labels of the test dataset.
+        category_names (list): The list of category names.
+
+    """
+    Y_pred = model.predict(X_test)
+    Y_pred = pd.DataFrame(Y_pred, columns = Y_test.columns)
+
+    for column in category_names:
+        print("Report for category:", column)
+        print(classification_report(Y_test[column], Y_pred[column]))
 
 
 def save_model(model, model_filepath):
+    """
+    Save model to a file using pickle.
+
+    Parameters:
+        model: The trained machine learning model to be saved.
+        model_filepath (str): Filepath to save the model.
+
+    """
     pickle.dump(model, open(model_filepath, 'wb'))
 
 
